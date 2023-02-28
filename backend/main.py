90b2e89d771169329203fbdab18de6b49d20af8a
HbotondS <hbotond97@gmail.com>
2023-02-28 23:59:05 +0200

    added backend service

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+cors = CORS(app, origins=["http://localhost:4200"])
+client = MongoClient("mongodb://localhost:60000/")
+db = client['todo-app']
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Flask is running'
+
+@app.route('/getTodos', methods=['GET'])
+def getTodos():
+    todos = db.Todos.find({})
+    return jsonify([todo for todo in todos])
+
+# @app.route('/post', methods=['POST'])
+# def post(msg: str):
+    # todo: fix post request
+
+if __name__ == '__main__':
+    app.run(debug=True)
