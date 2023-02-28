ae01fdadfaed2eb32f18a8936c71403d4679deb8
HbotondS <hbotond97@gmail.com>
2023-02-28 15:51:28 +0200

    added todo component and routing

---
+++ frontend/src/app/todo/todo.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { TodoPageRoutingModule } from "./todo-routing.module";
+import { TodoPage } from "./todo.component";
+
+@NgModule({
+    imports: [TodoPageRoutingModule],
+    declarations: [TodoPage]
+})
+export class TodoPageModule {}
