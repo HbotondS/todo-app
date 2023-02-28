f36e3a61fe43107c00ad1d8b57da72652d663962
HbotondS <hbotond97@gmail.com>
2023-02-28 17:27:19 +0200

    added eslint

--- frontend/src/app/todo/todo.component.ts
+++ frontend/src/app/todo/todo.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
-export class TodoPage {
-}
+export class TodoPage {}
