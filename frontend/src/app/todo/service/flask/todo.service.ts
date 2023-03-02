29c1bbe7fa63fb29f6bdad94dbf9b45725a5234b
HbotondS <hbotond97@gmail.com>
2023-03-02 20:55:59 +0200

    moved service to separate folder

--- frontend/src/app/todo/service/flask/todo.service.ts
+++ frontend/src/app/todo/service/flask/todo.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Todo } from './todo.model';
+import { Todo } from '../../todo.model';
 
 @Injectable({ providedIn: 'root' })
 export class TodoService {
+  private readonly apiURL = 'api/flask';
+
   constructor(private http: HttpClient) {}
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(`api/flask/getTodos`);
+    return this.http.get<Todo[]>(`${this.apiURL}/getTodos`);
   }
 }
